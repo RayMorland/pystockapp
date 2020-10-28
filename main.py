086d569f680fb7df138ad04846677456e4ef82de
RayMorland <raymondmorland@gmail.com>
2020-10-27 22:11:43 -0700

    Work on main and added requirements

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
 print("Welcome to the app")
+plt.style.use('seaborn') 
+mpl.rcParams['font.family'] = 'serif'
+np.random.seed(1000)
+y = np.random.standard_normal(20)
+x = np.arange(len(y))
+plt.plot(x, y)
+plt.show()
