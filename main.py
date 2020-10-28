703de3a87f7dd6c0bce3fdf67afc8ee5be88c1a2
RayMorland <raymondmorland@gmail.com>
2020-10-27 22:28:26 -0700

    Work on adding classes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from people.person import Person
 
 print("Welcome to the app")
 plt.style.use('seaborn') 
@@ -9,4 +10,7 @@ np.random.seed(1000)
 y = np.random.standard_normal(20)
 x = np.arange(len(y))
 plt.plot(x, y)
-plt.show()
+# plt.show()
+    
+bill = Person('Bill', 100)
+print('{0}\'s height is {1}'.format(bill.name ,bill.height))
